d6e9e75b9a186f3a3c95709e417897a9cca70c0a
kardobustore <cybercomunity45@gmail.com>
2026-09-20 18:19:12 +0700

    config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://xmqxoczvjisasrysrkls.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://xmqxoczvjisasrysrkls.supabase.co";
 const SUPABASE_KEY = "sb_publishable_qOIpMPw5J3EetOy5WdyTOQ_Lj_Tx1oC";
